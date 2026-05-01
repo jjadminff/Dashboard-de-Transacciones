8e47b23209fd254709c0684816acfaa838ba7cf1
Juan Jose Ledezma <93667805+jjadminff@users.noreply.github.com>
2026-04-30 19:48:54 -0600

    Update transacciones_dashboard.py

--- transacciones_dashboard.py
+++ transacciones_dashboard.py
@@ -128,7 +128,6 @@ df = pd.DataFrame(data)
 
 if df.empty:
     st.warning("No se encontraron montos válidos en el mes en curso.")
-    st.stop()
 else:
     st.subheader("Detalle de transacciones")
     st.dataframe(df[['fecha','monto','moneda','detalle']])
